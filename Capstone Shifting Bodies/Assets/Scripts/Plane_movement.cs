1bf9fac53f331cc04bbf0dd5c63f37fc6c5456ba
Anushka C Nair (RIT Student) <acn3929@ad.rit.edu>
2018-08-29 16:47:45 -0400

    flight progress

--- Capstone Shifting Bodies/Assets/Scripts/Plane_movement.cs
+++ Capstone Shifting Bodies/Assets/Scripts/Plane_movement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Plane_movement : MonoBehaviour
 {
+    public float speed = 0.0f;
 
 	// Use this for initialization
 	void Start ()
@@ -14,7 +15,20 @@ public class Plane_movement : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate ()
     {
-        transform.position -= transform.forward * Time.deltaTime * 90.0f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            speed = Mathf.Lerp(speed, 50.0f, 0.01f);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            speed = Mathf.Lerp(speed, 0.0f, 0.01f);
+        }
+        transform.position += -transform.forward * Time.deltaTime * speed;
+        transform.position += transform.up * Time.deltaTime * speed / 20;
+        Vector3 moveCamTo = transform.position + transform.forward * 30.0f + Vector3.up * 5.0f;
+        Camera.main.transform.position = moveCamTo;
+        Camera.main.transform.LookAt(transform.position);
+
 
         transform.Rotate(Input.GetAxis("Vertical"), 0.0f, Input.GetAxis("Horizontal"));	
 	}
