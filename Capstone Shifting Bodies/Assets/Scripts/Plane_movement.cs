057f97f0adea882393ffc0853cdb64931d0ad3ca
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2018-08-29 00:08:04 -0400

    Init Plane Movement

---
+++ Capstone Shifting Bodies/Assets/Scripts/Plane_movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane_movement : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        transform.position -= transform.forward * Time.deltaTime * 90.0f;
+
+        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, Input.GetAxis("Horizontal"));	
+	}
+}
