d828b78ca03f6a76ae83dc0607d0316eac6ec16a
Erin McAnany <elm6208@rit.edu>
2018-08-29 21:48:40 -0400

    Implemented Lamp Movement
    Lamp can hop in 4 directions using WASD with adjustable hop length and height. Lamp can toggle light on and off with spacebar. Lamp is currently represented with a tall cube until we have a proper asset.

--- Capstone Shifting Bodies/Assets/Scripts/LampMovement.cs
+++ Capstone Shifting Bodies/Assets/Scripts/LampMovement.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class LampMovement : MonoBehaviour {
 
     // Lamp needs to be able to move by hopping, connect to a power source, and turn the light on and off
+    public Rigidbody rbody;
+    public int hopDistance;
+    public int hopHeight;
+    public GameObject light;
+    private bool isGrounded = false;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +18,56 @@ public class LampMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        
+
+        //hop a short distance in the direction of the key pressed
+        if(isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                rbody.velocity = new Vector3(0, hopHeight, hopDistance);
+                isGrounded = false;
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                rbody.velocity = new Vector3(-hopDistance, hopHeight, 0);
+                isGrounded = false;
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                rbody.velocity = new Vector3(0, hopHeight, -hopDistance);
+                isGrounded = false;
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                rbody.velocity = new Vector3(hopDistance, hopHeight, 0);
+                isGrounded = false;
+            }
+        }
 		
-	}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(light.activeSelf == true)
+            {
+                light.SetActive(false);
+            }
+            else
+            {
+                light.SetActive(true);
+            }
+            
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!isGrounded)
+        {
+            if(collision.gameObject.tag == "Ground")
+            {
+                isGrounded = true;
+            }
+        }
+    }
+
 }
